27cbf9987cc0efcaa0afd30a4004cde288cf4e01
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2015-03-03 18:05:03 -0300

    og:image fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,7 @@ module.exports = function (grunt) {
         src: [
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
-          '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
+          '<%= yeoman.dist %>/images/{,*/}bg*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
