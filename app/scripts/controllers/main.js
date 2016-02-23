0436d7e92ac27e54831cd57f5abac363d638a171
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2016-02-23 14:39:02 -0300

    Mudança na estrutura dos dados

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,55 +16,51 @@ angular.module('trackApp')
 
     (function(){
         if(!angular.isDefined($localStorage.done)) {
-            $localStorage.done = [];
+          $localStorage.done = [];
         }
-        
-        $http.get('scripts/data.json').
-            success(function(data) {
-                var dones = _.map(data.resources, function(resource){
-                    if(_.includes($localStorage.done, resource._id)) {
-                        resource.checked = true;
-                    }
-                    return resource;
-                });
 
-                data.sections = _.map(data.sections, function(section){
-                    section.resources = _.where(data.resources, {'section': section._id});
-                    return section;
-                });
-
-                $scope.sections = data.sections;
-            })
+        $http.get('scripts/data.json')
+          .success(function(sections) {
+            $scope.sections = _.map(sections, function(section){
+              section.links = _.map(section.links, function(resource){
+                if(_.includes($localStorage.done, resource.url)) {
+                  resource.checked = true;
+                }
+                return resource;
+              })
+              return section;
+            });
+          })
     }())
-    
+
     function getTime(duration){
-        var output = '';
+      var output = '';
 
-        var momentObject = moment.duration(duration, 'minutes');
-        var hours = momentObject.hours();
-        var minutes = momentObject.minutes();
+      var momentObject = moment.duration(duration, 'minutes');
+      var hours = momentObject.hours();
+      var minutes = momentObject.minutes();
 
-        if(hours == 1) {
-            output += hours + ' hora';
-        }
-        if(hours > 1) {
-            output += hours + ' horas';
-        }
-        if(hours > 0 && minutes > 0) {
-            output += ' e ';
-        }
-        if(minutes > 0) {
-            output += minutes + ' minutos';
-        }
-        return output;
+      if(hours == 1) {
+        output += hours + ' hora';
+      }
+      if(hours > 1) {
+        output += hours + ' horas';
+      }
+      if(hours > 0 && minutes > 0) {
+        output += ' e ';
+      }
+      if(minutes > 0) {
+        output += minutes + ' minutos';
+      }
+      return output;
     }
 
     function changeResource(r){
-        var index = $localStorage.done.indexOf(r._id);
-        if(!r.checked && index === -1){
-            $localStorage.done.push(r._id);
-        } else {
-            $localStorage.done.splice(index, 1);
-        }
+      var index = $localStorage.done.indexOf(r.url);
+      if(!r.checked && index === -1){
+        $localStorage.done.push(r.url);
+      } else {
+        $localStorage.done.splice(index, 1);
+      }
     }
   }]);
