9d1c1ded91933e6db76a90dd1485c08c431f5d4b
Jean Lucas de Carvalho <jeanlucasdecarvalho@gmail.com>
2022-01-22 20:50:05 -0300

    Updates

--- _next/static/WPaiEfR2VaMopSlXxmj2Q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/741c73da302d0083.css","static/chunks/pages/index-486f8eb0ec59d98e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
